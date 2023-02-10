8b1af93995a008a7fe21196a4f336494324ae919
DenisDule <57055019+DenisDule@users.noreply.github.com>
2023-02-10 12:30:17 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,35 @@
 chrome.runtime.onMessage.addListener( function (request, sender, sendResponse) {
     sendResponse('Hi from background Script file');
 })
+
+
+
+const defaultFilters = [
+    "*://*.doubleclick.net/*",
+    "*://partner.googleadservices.com/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.google-analytics.com/*",
+    "*://creative.ak.fbcdn.net/*",
+    "*://*.adbrite.com/*",
+    "*://*.exponential.com/*",
+    "*://*.quantserve.com/*",
+    "*://*.scorecardresearch.com/*",
+    "*://*.zedo.com/*",
+]
+
+let blockedUrlCounter = 0;
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+        blockedUrlCounter++;
+        return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
+
+
+setInterval(() => {
+    console.log(`Number of blocked URLs: ${blockedUrlCounter}`);
+}, 1000);
+
+console.log(`ERR_BLOCKED_BY_CLIENT has occurred ${errBlockedByClientCounter} times.`);
